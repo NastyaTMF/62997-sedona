cdbd206da6c8e6f817f3f6234f972c617c7dbac9
NastyaTMF <onvard@gmail.com>
2015-12-15 14:08:13 +0300

    Автоматизация - cssmin, cmq, less

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,44 +3,46 @@
 module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt);
 
-  var config = {
-    pkg: grunt.file.readJSON("package.json"),
-
-    less: {
-      style: {
-        files: {
-          "css/style.css": "less/style.less"
-        }
-      }
-    },
-
-    postcss: {
-      options: {
-        processors: [
-          require("autoprefixer")({browsers: "last 2 versions"})
-        ]
+  grunt.initConfig({
+      less: {
+          style: {
+              files: {
+                  "css/style.css": ["less/style.less"]
+              }
+          }
       },
-      style: {
-        src: "css/*.css"
+      cmq: {
+         style: {
+             files: {
+                 "css/style.css": ["css/style.css"]
+             }
+         }
+      },
+      cssmin: {
+          options: {
+              keepSpecialComments: 0,
+              report: "gzip"
+          },
+          style: {
+              files: {
+                  "css/style.min.css": ["css/style.css"]
+              }
+          }
+      },
+      postcss: {
+          options: {
+              processors: [
+                  require("autoprefixer")({browsers: "last 2 versions"})
+              ]
+          },
+          style: {
+              src: "css/style.css"
+          }
       }
-    },
-
-    watch: {
-      style: {
-        files: ["less/**/*.less"],
-        tasks: ["less", "postcss"],
-        options: {
-          spawn: false,
-          livereload: true
-        }
-      }
-    }
-  };
-  
-
-
+  });
+          
   // Не редактируйте эту строку
   config = require("./.gosha")(grunt, config);
 
-  grunt.initConfig(config);
+  
 };
