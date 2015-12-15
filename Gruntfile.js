dc9529f66cdf58ff57f6b21b8f6c529d9c7ce6d1
NastyaTMF <onvard@gmail.com>
2015-12-15 17:39:54 +0300

    Отредактировала Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,45 @@
 module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt);
 
+  var config = {
+    pkg: grunt.file.readJSON("package.json"),
+
+    less: {
+      style: {
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")({browsers: "last 2 versions"})
+        ]
+      },
+      style: {
+        src: "css/*.css"
+      }
+    },
+
+    watch: {
+      style: {
+        files: ["less/**/*.less"],
+        tasks: ["less", "postcss"],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    }
+  };
+  
+
+
+  // Не редактируйте эту строку
+  config = require("./.gosha")(grunt, config);
+
   grunt.initConfig({
       less: {
           style: {
@@ -44,17 +83,19 @@ module.exports = function(grunt) {
               files: ["less/**/*.less"],
               tasks: ["less", "cmq", "postcss"]
           }
+      },
+      csscomb: {
+          style: {
+              expand: true,
+              src: ["less/**/*.less"]
+          }
       }
   });
   grunt.registerTask("build", [
       "less",
       "cmq",
       "postcss",
-      "cssmin"
+      "cssmin",
+      "csscomb"
   ]);
-          
-  // Не редактируйте эту строку
-  config = require("./.gosha")(grunt, config);
-
-  
 };
