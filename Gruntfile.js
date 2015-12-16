a989902c41645322b60e26ca61fd7e2bc31caa11
NastyaTMF <onvard@gmail.com>
2015-12-16 10:53:37 +0300

    Csscomb

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,6 +78,12 @@ module.exports = function(grunt) {
               tasks: ["less", "cmq", "postcss"]
           }
       },
+      csscomb: {
+          style: {
+              expand: true,
+              src: ["less/**/*.less"]
+          }
+      }
   });
   grunt.registerTask("build", [
       "less",
